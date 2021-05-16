b43ae69f96ec37e4636e2169ab4d5cd6309dceba
mr.Escow <escowxbox@gmail.com>
2021-05-17 01:05:19 +0300

    3

--- main.cpp
+++ main.cpp
@@ -23,29 +23,46 @@ int exponentiation(uint16_t number, int degree) {
 		return number * exponentiation(number, degree - 1);
 }
 // 3.–екурсивно, использу€ свойство чЄтности степени (то есть, если степень, в которую нужно возвести число, чЄтна€, основание возводитс€ в квадрат, а показатель делитс€ на два, 
-// а если степень нечЄтна€ - результат умножаетс€ на основание, а показатель уменьшаетс€ на единицу)
-
+//   а если степень нечЄтна€ - результат умножаетс€ на основание, а показатель уменьшаетс€ на единицу)
+int bin_pow(int number, int degree) {
+	if (degree == 0) return 1;
+	if (degree & 1) {
+		return bin_pow(exponentiation(number,2), degree / 2);		 
+	}
+	else {
+		return bin_pow(number, degree - 1) * number;
+	}
+}
 // 4.–еализовать нахождение количества маршрутов шахматного корол€ с преп€тстви€ми (где единица - это наличие преп€тстви€, а ноль - свободна€ дл€ хода клетка)
 
 
 int main() {
 	uint16_t number;
 	int degree;
+	//do {
+	//	cout << "\nTask_1\nInput 0 for exit.\nInput number :  ";
+	//	cin >> number;
+	//	if (number == 0) break;
+	//	cout << "Bin number :  ";
+	//	Dex_to_Bin(number);
+	//	cout << endl;
+	//} while (number != 0);
+	//do {
+	//	cout << "\nTask_2\nInput 0 for exit.\nInput number :  ";
+	//	cin >> number;
+	//	cout << "Input degree :  ";
+	//	cin >> degree;
+	//	if (number == 0) break;
+	//	cout << "Exponentiation :  "<< exponentiation(number, degree);
+	//	cout << endl;
+	//} while (number != 0);
 	do {
-		cout << "\nTask_1\nInput 0 for exit.\nInput number :  ";
-		cin >> number;
-		if (number == 0) break;
-		cout << "Bin number :  ";
-		Dex_to_Bin(number);
-		cout << endl;
-	} while (number != 0);
-	do {
-		cout << "\nTask_2\nInput 0 for exit.\nInput number :  ";
+		cout << "\nTask_3\nInput 0 for exit.\nInput number :  ";
 		cin >> number;
 		cout << "Input degree :  ";
 		cin >> degree;
 		if (number == 0) break;
-		cout << "Exponentiation :  "<< exponentiation(number, degree);
+		cout << "Binary exponentiation :  " << bin_pow(number, degree);
 		cout << endl;
 	} while (number != 0);
 	system("pause");
